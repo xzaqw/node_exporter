0f66d63bb930366d205e02d2c566fd779ffbb24a
mkonnov <markkonnov@gmail.com>
2024-08-17 08:44:55 +0400

    Exclude cpu_solaris from build -> no node_cpu metrics

--- collector/cpu_solaris.go
+++ collector/cpu_solaris.go
@@ -11,8 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !nocpu
-// +build !nocpu
+//go:build exclude
 
 package collector
 
